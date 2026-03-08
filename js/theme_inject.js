588672a6baa245b7cab0d26f64c47ba1adaeddca
lifeee <1244753906@qq.com>
2026-03-08 16:30:25 +0800

    Site updated: 2026-03-08 16:30:24

---
+++ js/theme_inject.js
@@ -0,0 +1,6 @@
+// 教程 https://dnocm.com/articles/beechnut/hexo-next-injects/  https://theme-next.js.org/docs/advanced-settings/injects#Examples
+
+// 不太行 先注释了
+// hexo.extend.filter.register('theme_inject', function(injects) {
+//     injects.bodyEnd.raw('load-custom-js', 'source/_data/theme_inject.swig', {}, {font_js: hexo.theme.theme_inject.enable});
+//   });
